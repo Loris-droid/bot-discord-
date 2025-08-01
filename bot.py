38d31144032cfd8d5584a74ac086a125bc5ba0f5
Loris-droid <lorishuon8@gmail.com>
2025-08-01 17:44:40 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+# Charger les variables du .env
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Connecté en tant que {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
